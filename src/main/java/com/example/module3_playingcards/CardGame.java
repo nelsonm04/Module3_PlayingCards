b04ad0f5d7064764f6ea0aed2266bad61bab39ed
Nelson Mojica <nelsonm43@icloud.com>
2025-03-09 17:22:18 -0400

    init

--- src/main/java/com/example/module3_playingcards/CardGame.java
+++ src/main/java/com/example/module3_playingcards/CardGame.java
@@ -6,12 +6,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class CardGame extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CardGame.class.getResource("CardGame.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 707, 455);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("CardGame.css")).toExternalForm());
         stage.setTitle("Card Game");
         stage.setScene(scene);
         stage.setResizable(false);
